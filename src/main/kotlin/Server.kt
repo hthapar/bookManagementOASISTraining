4874bb67ff08cfbc3fdfa93319877f8b91b74361
hthapar <himanshu.thapar@springernature.com>
2020-02-24 11:29:25 +0530

    Fetched Pen Details using Id By converting List to Map

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,10 +1,15 @@
 import db.books
+import db.pens
 import org.http4k.core.*
 
 val server: HttpHandler = { req: Request -> Response(Status.OK)
                             .body("${getBookNameUsingBookId(req.query("id")?.toInt())}") }
 
-private fun getBookNameUsingBookId(bookId: Int?): String? {
-    return books[bookId]
-}
+val serverPen: HttpHandler = { req: Request -> Response(Status.OK)
+                            .body("${getPenDetails(req.query("penId")?.toInt())}") }
+
+
+private fun getBookNameUsingBookId(bookId: Int?)= books[bookId]
+
+private fun getPenDetails(penId: Int?) = pens.toMap()[penId]
 
--- src/test/kotlin/com/test/springernature/PenDetailsTest.kt
+++ src/test/kotlin/com/test/springernature/PenDetailsTest.kt
@@ -2,27 +2,26 @@ package com.test.springernature
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Method
-import org.http4k.core.Request
+import db.pens
+import org.http4k.core.*
 
 import org.junit.jupiter.api.Test
+import serverPen
 
-class PenDetails {
+class PenDetailsTest {
 
     @Test
     fun `Should fetch pen name using Id`(){
 
-        val request = Request(Method.GET, "/").query("id", "1")
+        val request = Request(Method.GET, "/").query("penId", "4")
 
         val expected = "Cello Pen"
 
-        val actual = getPenDetails()
+        val actual = serverPen(request).bodyString() //response
 
         assertThat(actual, equalTo(expected))
 
     }
 
-    private fun getPenDetails(): String? {
-        return "Cello Pen"
-    }
+
 }
