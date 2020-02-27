0ce67c11b43eab92a3f83fb2b3d780e2c42f2c62
hthapar <himanshu.thapar@springernature.com>
2020-02-27 12:01:55 +0530

    Added new properties of color and brand to PenDB

--- src/main/kotlin/Writer.kt
+++ src/main/kotlin/Writer.kt
@@ -1,26 +1,28 @@
 interface Writer {
     val name: String
+    val brand: String
+    val color: String
     fun decorator(): String
     fun write(text: String): String
 }
 
-abstract class AbstractWriter(override val name: String) : Writer {
+abstract class AbstractWriter(override val name: String, override val brand: String, override val color: String) : Writer {
 
-    override fun decorator(): String = "Write using $name\n********************\n{text}\n********************"
+    override fun decorator(): String = "Write using $name from $brand of $color color. \n********************\n{text}\n********************\n"
 
     override fun write(text: String): String = decorator().replace("{text}", text)
 
     fun print(text: String) = println(write(text))
 }
 
-class Pen(name: String): AbstractWriter(name)
+class Pencil(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
 
-class SketchPen(name: String) : AbstractWriter(name)
+class Pen(name: String, brand : String, color: String): AbstractWriter(name,brand,color)
 
-class Pencil(name: String) : AbstractWriter(name)
+class SketchPen(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
 
-class Marker(name: String) : AbstractWriter(name)
+class Marker(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
 
-class WhiteBoardMarker(name: String) : AbstractWriter(name)
+class WhiteBoardMarker(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
 
-class PermanentMarker(name: String) : AbstractWriter(name)
+class PermanentMarker(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
--- src/main/kotlin/db/penDB.kt
+++ src/main/kotlin/db/penDB.kt
@@ -10,34 +10,34 @@ import Writer
 
 val pens:Map<Int,Writer> = mapOf (
 
-    1 to Pen("Lexi Pen"),
-    2 to Pen("Luxor Pen"),
-    3 to Pen("Parker Pen"),
-    4 to Pen("Cello Pen"),
+    1 to Pen("Lexi Pen", "Lexi","Blue"),
+    2 to Pen("Luxor Pen","Luxor","Blue"),
+    3 to Pen("Parker Pen","Parker","Blue"),
+    4 to Pen("Cello Pen","Cello","Blue"),
 
-    5 to SketchPen("Reynolds Sketch Pen"),
-    6 to SketchPen("Faber Sketch Pen"),
-    7 to SketchPen("Cello Sketch Pen"),
-    8 to SketchPen("Color lib Sketch Pen"),
+    5 to SketchPen("Reynolds Sketch Pen","Reynolds","Blue"),
+    6 to SketchPen("Faber Sketch Pen","Reynolds","Blue"),
+    7 to SketchPen("Cello Sketch Pen","Reynolds","Blue"),
+    8 to SketchPen("Color lib Sketch Pen","Reynolds","Blue"),
 
-    9 to Pencil("Apsara Pencil"),
-    10 to Pencil("DOMS Pencil"),
-    11 to Pencil("Nataraj Pencil"),
-    12 to Pencil("Camlin Pencil"),
+    9 to Pencil("Apsara Pencil","Reynolds","Blue"),
+    10 to Pencil("DOMS Pencil","Reynolds","Blue"),
+    11 to Pencil("Nataraj Pencil","Reynolds","Blue"),
+    12 to Pencil("Camlin Pencil","Reynolds","Blue"),
 
-    13 to Marker("Reynolds Marker"),
-    14 to Marker("Camlin Marker"),
-    15 to Marker("DOMS Marker"),
-    16 to Marker("Luxor Marker"),
+    13 to Marker("Reynolds Marker","Reynolds","Blue"),
+    14 to Marker("Camlin Marker","Reynolds","Blue"),
+    15 to Marker("DOMS Marker","Reynolds","Blue"),
+    16 to Marker("Luxor Marker","Reynolds","Blue"),
 
-    17 to WhiteBoardMarker("Camlin White Board Marker"),
-    18 to WhiteBoardMarker("Doms White Board Marker"),
-    19 to WhiteBoardMarker("Faber White Board Marker"),
-    20 to WhiteBoardMarker("Reynolds White Board Marker"),
+    17 to WhiteBoardMarker("Camlin White Board Marker","Reynolds","Blue"),
+    18 to WhiteBoardMarker("Doms White Board Marker","Reynolds","Blue"),
+    19 to WhiteBoardMarker("Faber White Board Marker","Reynolds","Blue"),
+    20 to WhiteBoardMarker("Reynolds White Board Marker","Reynolds","Blue"),
 
-    21 to PermanentMarker("Reynolds Permanent Marker"),
-    22 to PermanentMarker("Faber Permanent Marker"),
-    23 to PermanentMarker("Doms Permanent Marker"),
-    24 to PermanentMarker("Camlin Permanent Marker")
+    21 to PermanentMarker("Reynolds Permanent Marker","Reynolds","Blue"),
+    22 to PermanentMarker("Faber Permanent Marker","Reynolds","Blue"),
+    23 to PermanentMarker("Doms Permanent Marker","Reynolds","Blue"),
+    24 to PermanentMarker("Camlin Permanent Marker","Reynolds","Blue")
 
 )
