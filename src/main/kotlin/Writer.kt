e89bf9a4110146d97b42ef7d5995ff0fd16400a9
hthapar <himanshu.thapar@springernature.com>
2020-03-02 15:07:32 +0530

    'quantity and price' properties added in penDB and testDataDb

--- src/main/kotlin/Writer.kt
+++ src/main/kotlin/Writer.kt
@@ -2,25 +2,70 @@ interface Writer {
     val name: String
     val brand: String
     val color: String
+    val availability: Int
+    val price: Double
     fun decorator(): String
     fun write(text: String): String
 }
 
-abstract class AbstractWriter(override val name: String, override val brand: String, override val color: String) : Writer {
+abstract class AbstractWriter(
+    override val name: String,
+    override val brand: String,
+    override val color: String,
+    override val availability: Int,
+    override val price: Double
+) : Writer {
 
-    override fun decorator(): String = "Write using $name from $brand of $color color. \n********************\n{text}\n********************\n"
+    override fun decorator(): String =
+        "Write using $name from $brand of $color color. \n********************\n{text}\n********************\n"
 
     override fun write(text: String): String = decorator().replace("{text}", text)
 }
 
-class Pencil(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
+class Pencil(
+    name: String,
+    brand: String,
+    color: String,
+    availability: Int,
+    price: Double
+) : AbstractWriter(name, brand, color, availability, price)
 
-class Pen(name: String, brand : String, color: String): AbstractWriter(name,brand,color)
+class Pen(
+    name: String,
+    brand: String,
+    color: String,
+    availability: Int,
+    price: Double
+) : AbstractWriter(name, brand, color, availability, price)
 
-class SketchPen(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
+class SketchPen(
+    name: String,
+    brand: String,
+    color: String,
+    availability: Int,
+    price: Double
+) : AbstractWriter(name, brand, color, availability, price)
 
-class Marker(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
+class Marker(
+    name: String,
+    brand: String,
+    color: String,
+    availability: Int,
+    price: Double
+) : AbstractWriter(name, brand, color, availability, price)
 
-class WhiteBoardMarker(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
+class WhiteBoardMarker(
+    name: String,
+    brand: String,
+    color: String,
+    availability: Int,
+    price: Double
+) : AbstractWriter(name, brand, color, availability, price)
 
-class PermanentMarker(name: String, brand : String, color: String) : AbstractWriter(name,brand,color)
+class PermanentMarker(
+    name: String,
+    brand: String,
+    color: String,
+    availability: Int,
+    price: Double
+) : AbstractWriter(name, brand, color, availability, price)
